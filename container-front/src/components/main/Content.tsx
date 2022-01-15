7190e3c3375990f0a220cf67c7af6c91b8a937aa
Alex Dragan <35031007+aldragan0@users.noreply.github.com>
2022-01-15 23:54:37 +0200

    Extract the authentication logic from table

--- container-front/src/components/main/Content.tsx
+++ container-front/src/components/main/Content.tsx
@@ -1,16 +1,27 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { Loading } from "../Loading";
 
 //@ts-ignore
 const Table = React.lazy(() => import("pin/Table"));
 
-export default () => {
+export default (props: { homeRoute: string }) => {
+  // TODO: uncomment this once the authentication in place
+  // const navigate = useNavigate();
+  // const token = localStorage.getItem("token");
+
+  // if (!token) {
+  //   navigate(props.homeRoute);
+  //   return null;
+  // }
+  const token = "";
+
   return (
     <React.Fragment>
       <div style={{ minHeight: "100px" }} />
       <div style={{ flex: 1 }}>
         <React.Suspense fallback={<Loading />}>
-          <Table />
+          <Table token={token} />
         </React.Suspense>
       </div>
     </React.Fragment>
--- pin-front/src/components/Table.tsx
+++ pin-front/src/components/Table.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import { PageType } from "../types/PageType";
 import { Pin } from "./Pin";
 
@@ -11,24 +10,15 @@ async function api<T>(url: string, params: any): Promise<T> {
   return await (response.json() as Promise<T>);
 }
 
-export default () => {
-  // TODO: uncomment after main navigation and security in place
-  // const navigate = useNavigate();
-  const [isAuth, setIsAuth] = useState(false);
+export default (props: { token: string }) => {
+  const [_, setIsAuth] = useState(false);
   const [data, setData] = useState<PageType[]>([]);
 
   const loadData = async () => {
-    const token = localStorage.getItem("token");
-
-    // if (!token) {
-    //   navigate('/');
-    //   return null;
-    // }
-
     const currentPage: PageType = await api(
       `http://localhost:5020/pins?pageNo=1`,
       {
-        // headers: { 'Authorization': `Bearer ${token}` }
+        headers: { Authorization: `Bearer ${props.token}` },
       }
     );
 
