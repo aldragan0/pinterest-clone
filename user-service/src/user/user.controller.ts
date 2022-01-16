4a8f9946e9ec932ebb7809bc0bba4fd5fd6681a6
Alex Dragan <35031007+aldragan0@users.noreply.github.com>
2022-01-16 16:49:12 +0200

    Update signup method type

--- user-service/src/user/user.controller.ts
+++ user-service/src/user/user.controller.ts
@@ -1,14 +1,20 @@
-import { Body, Controller, HttpException, HttpStatus, Logger, Post, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpException,
+  HttpStatus,
+  Logger,
+  Post,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { MessagePattern } from '@nestjs/microservices';
 import { User } from './user.entity';
 import { InsertResult } from 'typeorm';
+import { UserInterface } from './user.interface';
 
 @Controller('user')
 export class UserController {
-  constructor(
-    private readonly userService: UserService
-  ) { }
+  constructor(private readonly userService: UserService) {}
 
   @MessagePattern({ role: 'user', cmd: 'get' })
   getUser(data: any): Promise<User> {
@@ -16,16 +22,16 @@ export class UserController {
   }
 
   @Post('signup')
-  async signUp(@Body() user: User): Promise<InsertResult> {
+  async signUp(@Body() user: Partial<UserInterface>): Promise<InsertResult> {
     try {
       return this.userService.createUser({
         email: user.email,
         username: user.email.split('@')[0],
-        password: user.password
+        password: user.password,
       });
     } catch (e) {
       Logger.log(e);
-      throw new HttpException("Bad Request", HttpStatus.BAD_REQUEST)
+      throw new HttpException('Bad Request', HttpStatus.BAD_REQUEST);
     }
   }
 }
